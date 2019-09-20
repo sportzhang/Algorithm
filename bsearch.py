d311a3bb2f526f79a372251737897ba4698cfc1c
KAIKAIZHANG <15927252186@163.com>
2019-09-20 16:44:13 +0800

    Add base function.

--- bsearch.py
+++ bsearch.py
@@ -2,6 +2,24 @@
 from typing import List
 
 
+def bsearch_base(nums, target):
+    """
+    :param nums: 待查找数列
+    :param target: 查找目标
+    :return: 查找结果
+    """
+    low, high = 0, len(nums) - 1
+    while low <= high:
+        mid = low + (high - low)//2
+        if nums[mid] == target:
+            return nums[mid]
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return "Not Found"
+
+
 def bsearch(nums: List, target: int) -> int:
     """
     :param nums: 待查找数列
