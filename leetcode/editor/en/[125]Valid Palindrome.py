7db03676d1a3e0ce6335718156525463b67b8857
KAIKAIZHANG <15927252186@163.com>
2021-03-23 10:32:16 +0800

    125 验证回文串

---
+++ leetcode/editor/en/[125]Valid Palindrome.py
@@ -0,0 +1,40 @@
+# Given a string s, determine if it is a palindrome, considering only alphanumer
+# ic characters and ignoring cases. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 2 * 105 
+#  s consists only of printable ASCII characters. 
+#  
+#  Related Topics Two Pointers String 
+#  👍 1829 👎 3674
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+# leetcode submit region end(Prohibit modification and deletion)
